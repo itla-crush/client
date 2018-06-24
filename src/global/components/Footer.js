188f882388a11d8ba92a82def3c4e55ff779e732
Neftaly Lugo <20163825@itla.edu.do>
2018-06-24 14:48:30 -0400

    commit

---
+++ src/global/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+        <footer id="footer" className="fixed-bottom">
+          <p>© 2018. Todos los Derechos Reservados por Social Crush.</p>
+        </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Components
+import Header from './global/components/Header';
+import Content from './global/components/Content';
+import Footer from './global/components/Footer';
+
+// Styles
+import './index.css';
+import './global/styles/bootstrap/css/bootstrap.min.css';
+import './global/styles/bootstrap/js/fontawesome-all.js';
+
+ReactDOM.render(<Header />, document.getElementById('header-root'));
+ReactDOM.render(<Content />, document.getElementById('content-root'));
+ReactDOM.render(<Footer />, document.getElementById('footer-root'));
