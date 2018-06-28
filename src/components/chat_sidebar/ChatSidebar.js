8e0d82d92dbad29e7cf3c07d1f9ed574716617d0
Neftaly Lugo <20163825@itla.edu.do>
2018-06-28 13:06:47 -0400

    Working in the structure of React

---
+++ src/components/chat_sidebar/ChatSidebar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+// Components
+import ChatItem from '../chat_item/ChatItem';
+
+// Assets
+import './chatsidebar.css';
+
+class ChatSidebar extends Component {
+    render() {
+      return (
+        <section className="ChatSidebar">
+          <div className>
+            {/* Chat Users */}
+            <div className="chatOrderedList">
+              <div className="chat-sidebar">
+                <ul className="list-unstyled">
+
+                  {/* Each User Chat */}
+                  <ChatItem />
+                  <ChatItem />
+                  <ChatItem />
+                  {/* /Each User Chat */}
+
+                </ul>
+              </div>
+            </div>
+            {/* /Chat Users */}
+          </div> 
+        </section>
+
+      );
+    }
+  }
+  
+  export default ChatSidebar;
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 // Components
-import Header from '../../components/header/Header';
+import Header from '../../components/header/Header.js';
+import UserSidebar from '../../components/user_sidebar/UserSidebar.js';
+import NewsFeed from '../../components/newsfeed/NewsFeed.js';
+import ChatSidebar from '../../components/chat_sidebar/ChatSidebar.js'
 
 // Assets
 import './home.css';
@@ -11,6 +14,17 @@ class Home extends Component {
       return (
         <div className="Home">
             <Header />
+            <div className="container main-content">
+                <aside className="left-aside center-content">
+                    <UserSidebar />
+                </aside>
+                <main className="main center-content">
+                    <NewsFeed />
+                </main>
+                <aside className="right-aside center-content">
+                    <ChatSidebar />
+                </aside>
+            </div>
         </div>
       );
     }
