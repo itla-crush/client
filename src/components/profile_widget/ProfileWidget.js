457dcedeb42f75df21459f205eb11fecf8bac95d
Neftaly Lugo <20163825@itla.edu.do>
2018-07-17 23:39:04 -0400

    Working in Profile Widget

--- src/components/profile_widget/ProfileWidget.js
+++ src/components/profile_widget/ProfileWidget.js
@@ -11,12 +11,26 @@ class ProfileWidget extends Component {
             <div className="modal-dialog modal-dialog-centered">
               <div className="modal-content">
 
-                <div className="profile-widget-content">
-                  Hola
-                </div>
-                <div className="profile-widget-footer">
-                  <button className="btn-o"><i class="fa fa-user-plus"></i> Seguir</button>
-                  <button className="btn-o"><i class="fa fa-user-plus"></i> Mensaje</button>
+                <div className="">
+                  <div className="profile-widget-header">
+                    <div className="div-user-img-profile">  {/* Contenedor de la Imagen de Perfil */}
+                      <a href="#">
+                        <img alt="" className="user-img-profile" src="https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0" /> {/* Imagen */}
+                      </a>
+                    </div>
+                    <div className="div-username"> 
+                      <a href="#">@anonimo</a> {/* Usuario */}
+                      <p>32 min</p>
+                    </div>
+                  </div>
+                  <div className="profile-widget-content">
+                    Hola
+                  </div>
+                  <div className="profile-widget-footer center-content">
+                    <button className="btn-o"><i class="fa fa-user-plus"></i> Seguir</button>
+                    <button className="btn-o"><i class="fa fa-user-plus"></i> Mensaje</button>
+                    <button className="btn-o"><i class="fa fa-user-plus"></i> Dedicatoria</button>
+                  </div>
                 </div>
 
               </div>
