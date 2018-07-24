64188f7985691e18321d41d062a450edce448b64
Neftaly Lugo <20163825@itla.edu.do>
2018-07-24 15:39:32 -0400

    Working in SearchUser

--- src/components/result_widget/ResultWidget.js
+++ src/components/result_widget/ResultWidget.js
@@ -39,6 +39,23 @@ class ResultWidget extends Component {
       const users = this.props.users;
       console.log(users);
       var listItems;
+      if(users){
+        listItems = users.map((user) =>
+          <li key={user.id}>
+            <a href="#">
+              <div className="resultado-imagen">
+                <img src={user.photoUrl || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} alt="" />
+              </div>
+              <div className="resultado-nombres">
+                <p>{user.displayName || 'Username'}</p>
+                <p>{user.username || '@username'}</p>
+              </div>
+            </a>
+          </li>
+        );
+      } else {
+        listItems = <li>No hay usuarios</li>;
+      }
       return (
         <div className="wd"> 
           <div className="resultados">
@@ -49,12 +66,13 @@ class ResultWidget extends Component {
               <div className="busqueda-perfil">
                 {/* <UserList users={this.props.users} /> */}
                 {/* {this.UserList} */}
-                { users ? (
+                {/* { users ? (
                   console.log('If')
                   ) : (
                   console.log('Else')
                 )
-                }
+                } */}
+                <ul className="list-unstyled">{listItems}</ul>
               </div>
             </div>
           </div>
