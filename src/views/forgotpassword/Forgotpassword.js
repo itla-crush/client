b4e3de199ce03d2fed305eee68d3644e8bb309d0
Neftaly Lugo <20163825@itla.edu.do>
2018-07-30 01:57:12 -0400

    Comentario

--- src/views/forgotpassword/Forgotpassword.js
+++ src/views/forgotpassword/Forgotpassword.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import _ from 'lodash';
 
 import './forgotpassword.css';
 
 class Forgotpassword extends Component {
+    constructor(props) {
+        super(props);
+        this.handleResetPassword = this.handleResetPassword.bind(this);
+    }
+    
+    handleResetPassword = () => {
+        var emailForgotPassword = document.getElementById('emailForgotPassword');
+        emailForgotPassword = _.trim(emailForgotPassword.value);
 
+        if(!_.isEmpty(emailForgotPassword)) {
+            firebase.auth().sendPasswordResetEmail('20163825@itla.edu.do').then(() => {
+                //   Password reset email sent.
+                console.log('Password reset email sent.');
+                console.log('Chequea tu correo para restablecer contraseña');
+            })
+            .catch(error => {
+            // Error occurred. Inspect error.code.
+                switch (error) {
+                    case 'auth/invalid-email':
+                        console.log('El correo introducido no es válido.');
+                        console.log('El correo introducido no es válido.');
+                        break;
+                    case 'auth/user-not-found':
+                        console.log('Usuario no encontrado.');
+                        console.log('Usuario no encontrado.');
+                        break;
+                    default:
+                        console.log(error);
+                        break;
+                }
+                console.log('Error occurred. Inspect error.code.');
+            });
+
+        } else {
+            console.log('Debe escribir su correo');
+            alert('Debe escribir su correo');
+        }
+    }
     render(){
         return(
             <div className="ForgotPassword">
@@ -21,9 +60,9 @@ class Forgotpassword extends Component {
                         <form action="">
                             <div className="correo">
                                 <div className="cc"><label htmlFor="">Correo</label></div>
-                                <input type="email" placeholder=""/>
+                                <input id="emailForgotPassword" type="email" placeholder=""/>
                             </div>
-                           <div className="btn-s"><input type="submit" value="Enviar"/></div> 
+                           <div className="btn-s"><input onClick={this.handleResetPassword} type="submit" value="Enviar"/></div> 
                         </form>
                     </div>
                 </div>
