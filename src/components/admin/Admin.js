d4d6957946d917339dee30d5237b9edc20d9aa04
DaltonTejada <nowyousee2017@gmail>
2018-08-12 12:13:42 -0400

    Comentario

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// Assets
+import './admin.css'; 
+
+
+class Admin extends Component{
+    render(){
+        return(
+            <div>
+                <p>Hola mundo</p>
+            </div>
+        )
+    }
+}
+
+export default Admin;
--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,7 @@ import Landing from '../views/landing/Landing';
 import Profile from '../views/profile/Profile';
 import EditProfile from '../views/edit_profile/EditProfile';
 import Page404 from '../views/page404/Page404';
+import Admin from '../components/admin/Admin';
 // import Forgotpassword from '../components/forgotpassword/Forgotpassword';
 
 const AppRoute = () => {
@@ -21,6 +22,7 @@ const AppRoute = () => {
             {/* <Route path='/forgotpassword' component={Forgotpassword} /> */}
             <Route path='/profile/:id' component={Profile} />
             <Route path='/edit_profile/:id' component={EditProfile} />
+            <Route path='/admin' component={Admin} />
             {/* <Route path='/edit_profile?:id' component={EditProfile} /> */}
             <Route component={Page404} />
         </Switch>
