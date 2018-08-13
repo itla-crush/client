2db787f5944ef2dc70dcb6683e63877911b80f4b
DaltonTejada <nowyousee2017@gmail>
2018-08-13 17:47:47 -0400

    Comentario

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -7,8 +7,41 @@ import './admin.css';
 class Admin extends Component{
     render(){
         return(
-            <div>
-                <p>Hola mundo</p>
+            <div className="ad">
+               <div className="administrador">
+                   <div className="tablero-title">
+                       <p>Tablero</p>
+                   </div>
+                   {/* estadisticas usuarios */}
+                   <div className="estadisticas">
+
+                       <div className="est-users">
+                         <i class="fas fa-users"></i>
+                         <div className="info-t">
+                             <h4 className="tl">Usuarios</h4>
+                             <p className="est">80</p>
+                         </div>
+                       </div>
+                         {/* estadisticas declaraciones */}
+                       <div className="est-declaraciones">
+                         <i class="fas fa-chart-pie"></i>
+                         <div className="info-t">
+                             <h4 className="tl">Publicaciones</h4>
+                             <p className="est">140</p>
+                         </div>
+                       </div>
+
+                         {/* estadisticas declaraciones reportadas */}
+                       <div className="est-declaraciones-report">
+                         <i class="far fa-bell"></i>
+                         <div className="info-t">
+                             <h4 className="tl">Publicaciones reportadas</h4>
+                             <p className="est">27</p>
+                         </div>
+                       </div>
+                   </div>
+
+               </div>
             </div>
         )
     }
