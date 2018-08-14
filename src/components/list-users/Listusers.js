6f2ec20372c86f9d44f1222f7157a61912ff194a
DaltonTejada <nowyousee2017@gmail>
2018-08-14 07:12:50 -0400

    Comentario

--- src/components/list-users/Listusers.js
+++ src/components/list-users/Listusers.js
@@ -32,10 +32,9 @@ class Listusers extends Component{
                         <input id="" type="text" className="search-u" placeholder="Buscar"/>
                     </div>
                     <div className="results-tab">
-                        <div className="container">
-                        <div className="row">
+                      
 
-                         <div className="pf-ct col-md-4">
+                         <div className="pf-ct">
                             <section>
                                 <div className="ust">
                                    <h4>Luisa Avalo</h4> 
@@ -53,8 +52,154 @@ class Listusers extends Component{
                             </section>
                          </div> 
 
-                            </div>
-                          </div>
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+                          <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+
+                         <div className="pf-ct">
+                            <section>
+                                <div className="ust">
+                                   <h4>Luisa Avalo</h4> 
+                                </div>
+                                <div className="imgXUPR center-content">  {/* Contenedor de la Imagen de Perfil */}
+                                    <a href="#" onClick={this.handleOnClick} >
+                                    <img alt="" className="imgXUPR-2 " src={this.props.photoUrl  || 'https://firebasestorage.googleapis.com/v0/b/social-crush.appspot.com/o/images%2Fuser_profile%2Fprofile_placeholder.jpg?alt=media&token=7efadeaa-d290-44aa-88aa-ec18a5181cd0'} /> {/* Imagen */}
+                                    </a>
+                                </div>
+                                <div className="center-content">
+                                    <a className="username" href="#" onClick={this.handleOnClick}>
+                                    <h5>@Luisa85</h5> 
+                                    </a>
+                                </div>
+                            </section>
+                         </div> 
+
+                      
                     </div>
                 </div>
                 
